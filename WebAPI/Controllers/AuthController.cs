281fd773901b3ac0337fcf356190fcc776ff1cdd
Vullet07 <vasil.rusev.of@gmail.com>
2025-12-16 14:01:35 +0200

    added /me endpoint

--- WebAPI/Controllers/AuthController.cs
+++ WebAPI/Controllers/AuthController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Services.AuthUserService;
 using Services.Dtos;
 using Services.JwtService;
 
@@ -16,12 +17,14 @@ namespace WebAPI.Controllers
     {
         private readonly ITokenService _tokenService;
         private readonly IPasswordHasher<User> _passwordHasher;
+        private readonly IAuthUserService _authUserService;
         private readonly AppDbContext _db;
 
-        public AuthController(ITokenService tokenService, IPasswordHasher<User> passwordHasher, AppDbContext db)
+        public AuthController(ITokenService tokenService, IPasswordHasher<User> passwordHasher, IAuthUserService authUserService, AppDbContext db)
         {
             _tokenService = tokenService;
             _passwordHasher = passwordHasher;
+            _authUserService = authUserService;
             _db = db;
         }
 
@@ -73,6 +76,22 @@ namespace WebAPI.Controllers
                 Username = user.Username,
                 Role = user.Role,
                 PhotoUrl = user.PhotoUrl
+            });
+        }
+       
+        [Authorize]
+        [HttpGet("me")]
+        public ActionResult<UserDto> Me()
+        {
+            if (!_authUserService.IsAuthenticated || _authUserService.Id == null)
+                return Unauthorized();
+
+            return Ok(new UserDto
+            {
+                Id = _authUserService.Id.Value,
+                Username = _authUserService.Username!,
+                Role = _authUserService.Role ?? Role.Student,
+                PhotoUrl = null
             });
         }
     }
